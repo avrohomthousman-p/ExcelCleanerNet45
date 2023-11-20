c60d8778f3fbd00bf413bc419aa08a3e984fdec0
avrohomthousman <avrohomthousman@gmail.com>
2023-11-20 13:57:12 -0500

    started rebuilding the formula generator for VendorInvoiceReport

--- ExcelCleanerNet45/FormulaGeneration/PeriodicFormulasOnTop.cs
+++ ExcelCleanerNet45/FormulaGeneration/PeriodicFormulasOnTop.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using OfficeOpenXml;
 
 namespace ExcelCleanerNet45.FormulaGeneration
 {
+
+    public delegate bool IsSummaryCell(ExcelRange cell);
+    public delegate bool IsKeyOfSection(ExcelRange cell);
+
+
+
+
     /// <summary>
     /// Implementation of IFormulaGenerator that adds formulas to the top of "sections" found inside data columns
     /// of the worksheet. A section is defined as a series of data cells that all corrispond to a single "key" which 
@@ -15,42 +20,158 @@ namespace ExcelCleanerNet45.FormulaGeneration
     /// This class is a lot like the PeriodicFormulaGenerator, but it puts the formulas on the top of each section
     /// instead of the bottom.
     /// 
-    /// The first string in the list of arguments for this class should follow this pattern: r=[insert regex] 
-    /// Where the regex should match the keys for each section. After that argument, the titles of each data column 
-    /// that need formulas should be passed in as well (meaning, which columns need formulas at the top of each section).
+    /// The string arguments for this class should be the titles of each data column that needs
+    /// formulas (meaning, the header text that can be found at the top of each column that needs formulas).
     /// </summary>
-    internal class PeriodicFormulasOnTop : PeriodicFormulaGenerator
+    internal class PeriodicFormulasOnTop : IFormulaGenerator
     {
-        //TODO
+        private IsDataCell isDataCell = new IsDataCell(FormulaManager.IsDollarValue); //default implementation
+        private IsSummaryCell isSummaryCell = new IsSummaryCell(
+            (cell => FormulaManager.IsDollarValue(cell) && cell.Style.Font.Bold));    //defualt implementation
 
-        protected override void ProcessFormulaRange(ExcelWorksheet worksheet, ref int row, int dataCol)
+        private IsKeyOfSection cellHasKey = new IsKeyOfSection(
+            (cell => !FormulaManager.IsEmptyCell(cell) 
+                    && !FormulaManager.IsDollarValue(cell) 
+                    && cell.Style.Font.Bold));                                        //default implementation
+
+
+
+        protected int firstRowOfTable = -1;
+
+
+
+
+        public virtual void SetDataCellDefenition(IsDataCell isDataCell)
         {
-            int start = row; //the formula range starts here, at the first non-empty cell
-
-
-            //Now find the bottom of the formula range
-            AdvanceToLastRow(worksheet, ref row);
+            this.isDataCell = isDataCell;
+        }
 
 
 
-            //Borrowing this function from the parent class, and using it to find the summary cell/top of
-            //the NEXT section
-            int lastRow = FindSummaryCellRow(worksheet, row, start, dataCol);
-            if (lastRow == -1)
+        public virtual void SetSummaryCellDefenition(IsSummaryCell summaryCellDef)
+        {
+            this.isSummaryCell = summaryCellDef;
+        }
+
+
+
+
+        public virtual void SetSectionKeyDefenition(IsKeyOfSection predicate)
+        {
+            this.cellHasKey = predicate;
+        }
+
+
+
+
+        public void InsertFormulas(ExcelWorksheet worksheet, string[] headers)
+        {
+            if(headers.Length == 0)
             {
-                lastRow = worksheet.Dimension.End.Row;
+                return;
             }
 
 
+            FindStartOfTable(worksheet, headers[0]);
+
+            List<int> keys = FindAllSectionKeys(worksheet);
+
+            List<int> dataColumns = FindAllDataColumns(worksheet, headers);
 
 
-            //Insert formulas
-            ExcelRange summaryCell = worksheet.Cells[start, dataCol];
-            Console.WriteLine("adding formula to " + summaryCell.Address);
+            
 
-            string formula = FormulaManager.GenerateFormula(worksheet, start + 1, lastRow - 1, dataCol);
+            //TODO: complete this method
 
-            FormulaManager.PutFormulaInCell(summaryCell, formula, false);
+        }
+
+
+
+        /// <summary>
+        /// Finds the first row of the worksheet that is actually part of the table. Anything above that row is
+        /// considered a major header, and anything on it or below is either data or a minor header.
+        /// </summary>
+        /// <param name="worksheet">the worksheet in need of formulas</param>
+        /// <param name="header">the header to look for to signal the start of the table</param>
+        protected virtual void FindStartOfTable(ExcelWorksheet worksheet, string header)
+        {
+            ExcelIterator iter = new ExcelIterator(worksheet);
+
+            foreach(ExcelRange cell in iter.FindAllCells())
+            {
+                if(FormulaManager.TextMatches(cell.Text, header))
+                {
+                    firstRowOfTable = iter.GetCurrentRow();
+                    return;
+                }
+            }
+
+
+            throw new Exception("Formulas could not be added becuase no data was found in the worksheet.");
+        }
+
+
+
+
+        /// <summary>
+        /// Builds a list containing the row numbers where each key can be found. The last entry will be 
+        /// a reference to a row that is outside the table, indicating that the section before it spans 
+        /// untill the end of the table.
+        /// </summary>
+        /// <param name="worksheet">the worksheet in need of formulas</param>
+        /// <returns>a list of all rows where keys can be found</returns>
+        protected virtual List<int> FindAllSectionKeys(ExcelWorksheet worksheet)
+        {
+            ExcelIterator iter = new ExcelIterator(worksheet, firstRowOfTable, 1);
+
+
+            //find all the cells with keys
+            List<int> keys = iter.GetCells(ExcelIterator.SHIFT_DOWN)
+                        .Where(cell => cellHasKey(cell))
+                        .Select(cell => cell.Start.Row)
+                        .ToList();
+
+
+            keys.Add(worksheet.Dimension.End.Row + 1); //add a cell to represent the end of the last section
+
+            return keys;
+        }
+
+
+
+
+        /// <summary>
+        /// Finds all the columns in the table that require formulas in them
+        /// </summary>
+        /// <param name="worksheet">the worksheet in need of formulas</param>
+        /// <param name="headers">all the headers that will be found at the top of a column that needs formulas</param>
+        /// <returns>a list of all the columns that require formulas</returns>
+        protected virtual List<int> FindAllDataColumns(ExcelWorksheet worksheet, string[] headers)
+        {
+            ExcelIterator iter = new ExcelIterator(worksheet, firstRowOfTable, 1);
+
+
+
+            Func<ExcelRange, bool> matchesAHeader = cell => 
+            { 
+                foreach(string header in headers)
+                {
+                    if(FormulaManager.TextMatches(cell.Text, header))
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            };
+
+
+
+            //Get all columns with text matching one of the headers (and therefore need formulas)
+            return iter.GetCells(ExcelIterator.SHIFT_RIGHT)
+                        .Where(matchesAHeader)
+                        .Select(cell => cell.Start.Column)
+                        .ToList();
         }
     }
 }
